23f92b57b2961b6efef0cea280fbfd4e282e7a66
lonvali793 <ugai0429@gmail.com>
2025-03-25 21:23:36 +0900

    言語フォーマットのバリデーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,16 @@ class User < ApplicationRecord
          validates :surname_kana, presence: true
          validates :firstname_kana, presence: true
          validates :birthday, presence: true
-         validates :email, presence: true, format: { with: /@/, message: "@ を含めてください" }
          validates :password, presence: true, length: { minimum: 6}
+
+         with_options presence: true do
+          # ひらがな、カタカナ、漢字のみ許可する
+          validates :firstname, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+          validates :surname, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+          # カタカナのみ許可する
+          validates :firstname_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+          validates :surname_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
+          validates :password,  format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/, message: "is invalid. Password must contain at least one alphanumeric character."}
+        end
 end
 
