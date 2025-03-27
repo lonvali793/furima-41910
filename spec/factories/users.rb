a51d9c7fbfcdcf6bb956153b00129d95e16185f4
lonvali793 <ugai0429@gmail.com>
2025-03-27 20:47:29 +0900

    単体テストコード記載　言語指定

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,9 +5,9 @@ FactoryBot.define do
     password { Faker::Internet.password(min_length: 6) }  # 8文字以上のランダムなパスワード
     password_confirmation { password }  # 確認用パスワードも同じ値にする
 
-    surname { Faker::Name.last_name }        # ランダムな苗字
-    firstname { Faker::Name.first_name }     # ランダムな名前
-    surname_kana { "ヤマダ" }                 # ダミーのカナ
+    surname {Faker::Japanese::Name.last_name }       
+    firstname {  Faker::Japanese::Name.first_name }     
+    surname_kana { "ヤマダ" }            
     firstname_kana { "タロウ" }
     birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }  # 18歳〜65歳の誕生日をランダムに設定
   end
