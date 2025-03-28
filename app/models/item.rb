ca895a3c58be4af3aa747f1962d8b1966f701c36
lonvali793 <ugai0429@gmail.com>
2025-03-28 21:05:10 +0900

    マイグレート実施　バリデーション設定

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+belongs_to :user
+has_one :order
+
+validates :image, presence: true
+validates :item_name, presence: true
+validates :item_content, presence: true
+validates :item_category_id, presence: true
+validates :item_condition_id, presence: true
+validates :shipping_cost_id, presence: true
+validates :prefecture_id, presence: true
+validates :delivery_time_id, presence: true
+validates :price, presence: true
+end
