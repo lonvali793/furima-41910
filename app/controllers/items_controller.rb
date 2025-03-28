09e18da986f2312abd7e095e2d542f490089e0de
lonvali793 <ugai0429@gmail.com>
2025-03-28 21:16:08 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,3 +7,10 @@ class ItemsController < ApplicationController
   end
   
 end
+
+
+private
+
+def item_params
+  params.require(:item).permit(:item_name, :item_content, :item_category_id, :item_condition_id , :shipping_cost_id, :prefecture_id, :delivery_time_id, :price     )
+  end
