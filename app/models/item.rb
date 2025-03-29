b7d7103b35ef04e7c72e8191bab1cee11ee27ccf
lonvali793 <ugai0429@gmail.com>
2025-03-29 10:59:17 +0900

    ActiveHash 導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,17 @@ has_one_attached :image
 validates :image, presence: true
 validates :item_name, presence: true
 validates :item_content, presence: true
-validates :item_category_id, presence: true
+validates :item_category_id, numericality: { other_than: 1 ,message: "can't be blank"} 
 validates :item_condition_id, presence: true
 validates :shipping_cost_id, presence: true
 validates :prefecture_id, presence: true
 validates :delivery_time_id, presence: true
 validates :price, presence: true
+
+extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :delivery_time
 end
