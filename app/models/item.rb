2fdb5967d036dc6922fe176a0685248bf36eb195
lonvali793 <ugai0429@gmail.com>
2025-03-29 17:26:18 +0900

    出品に失敗するとエラーメッセージが表示される実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ belongs_to :user
 has_one :order
 has_one_attached :image
 
-validates :image, presence: true
+validates :image, presence: true 
 validates :item_name, presence: true
 validates :item_content, presence: true
 validates :item_category_id, numericality: { other_than: 1 ,message: "can't be blank"} 
