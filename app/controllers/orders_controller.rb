f9d10b6f261cb75fa094bec055d16c981a604216
lonvali793 <ugai0429@gmail.com>
2025-04-05 10:52:11 +0900

    フォームオブジェクトを記載

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,18 +2,32 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    
-    
-    @item = Item.find(params[:item_id])
+     @item = Item.find(params[:item_id])
     if current_user.id == @item.user_id
       redirect_to root_path
+      @order_address = OrderAddress.new
   end
 end
 
   def create
-  end
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
 
-end
+    else
+     render :index, status: :unprocessable_entity
+  end
+ end
 
 private
 
+def order_address_params
+params.require(:order_address).permit(
+  :postal_code, :prefecture, :city, :house_number,
+  :building_name,:phone_number
+).merge(user_id: current_user.id, item_id: @item.id)
+end
+
+end
